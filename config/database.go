d6c18b5f8949d7150f550af97c7b80a62b610509
dimpurw <terminatorid2@gmail.com>
2023-08-28 23:16:39 +0700

    Config DB

---
+++ config/database.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"restful-api/helper"
+
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "dev"
+	password = "secret"
+	dbName   = "test"
+)
+
+func DatabaseConnection() *sql.DB {
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password, dbName)
+
+	db,err := sql.Open("postgres",sqlInfo)
+	helper.PanicIfError(err)
+
+	err = db.Ping()
+	helper.PanicIfError(err)
+
+	log.Info().Msg("Connected to Database !")
+
+	return db
+}
